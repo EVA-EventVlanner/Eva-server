c8c1cf4af1c9398a66f909c7fe74a9b7922c8108
dawimuhammad <muhamadhaddawi@gmail.com>
2018-08-26 08:38:13 +0700

    test detect image after uploading to storage

--- routes/vision.js
+++ routes/vision.js
@@ -4,13 +4,18 @@ const Vision = require('../controllers/vision-controller')
 
 router.post('/analyze', Vision.Controller.analyze)
 
-router.post('/upload', Vision.multer.single('file'), Vision.Controller.uploadToStorage, function (req, res) {
-    res
-        .status(200)
-        .send ({
-            message: 'Upload successfull',
-            link: req.file.cloudStoragePublicUrl
-        })
-})
+// router.post('/upload', Vision.multer.single('file'), Vision.Controller.uploadToStorage, function (req, res) {
+//     res
+//         .status(200)
+//         .send ({
+//             message: 'Upload successfull',
+//             link: req.file.cloudStoragePublicUrl
+//         })
+// })
+
+router.post('/upload',
+                Vision.multer.single('file'),
+                Vision.Controller.uploadToStorage,
+                Vision.Controller.analyze )
 
 module.exports = router
