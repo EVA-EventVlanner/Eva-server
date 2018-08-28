0ea4d61a801354f215d55280c3c1245862f72d90
dawimuhammad <muhamadhaddawi@gmail.com>
2018-08-28 18:06:07 +0700

    add console print in static method analyze

--- controllers/vision-controller.js
+++ controllers/vision-controller.js
@@ -1,13 +1,10 @@
 const axios = require("axios");
 const vision = require("../middlewares/vision-logic");
-// const Item = require("../models/item-model");
-// const EventModel = require("../models/event-model");
-// const User = require("../models/user-model");
 require("dotenv").config();
 
 class Controller {
 	static async analyze (req, res, next) {
-		// let receipt = 'https://i.imgur.com/49QGMvx.jpg'
+
 		console.log('----------> Analyze image started .....')
 
 		let uri = `https://vision.googleapis.com/v1/images:annotate?key=${process.env.VISION_API_KEY}`
@@ -76,8 +73,6 @@ class Controller {
 			console.log('receipt url : ', receipt)
 		}
 
-		console.log('cloudstoragepublicurl : ', receipt)
-
 		try {
 			const response = await axios.post(uri, {
 				"requests": [
@@ -109,36 +104,6 @@ class Controller {
 				e
 			})
 		}
-	}
-
-	static downloadToServer (req, res, next) {
-		let fs = require('fs')
-		let request = require('request');
-
-		let fileName = 'receipt' + Date.now() + '.jpg'
-		
-		let download = function(uri, filename, callback){
-			request.head(uri, function(err, res, body){
-				console.log('content-type:', res.headers['content-type']);
-				console.log('content-length:', res.headers['content-length']);
-
-				request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-			})
-		}
-
-		download(req.body.image_url, fileName, function() {
-			console.log('done');
-			res.json({message: 'image download done'})
-		})
-		// let axios = require('axios')
-
-		// axios.get('https://firebasestorage.googleapis.com/v0/b/burogu-desu.appspot.com/o/receipt%2F1535275956074indomaret.jpg?alt=media&token=1e652fe6-6108-4db8-9faf-e0d5c4ff0313')
-		// .then (response => 
-		// 	res.json({response: response})
-		// )
-		// .catch(err => {
-		// 	res.json({ error: err.message})
-		// })
 	}
 }
 
